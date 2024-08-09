2019846ad36c31f24fc39610c0df2cb35cec74c7
Florecitaa <160565962+Florecitaa@users.noreply.github.com>
2024-08-08 20:33:19 -0600

    Crear habitacion cs

--- Pages/CrearHabitacion.aspx.cs
+++ Pages/CrearHabitacion.aspx.cs
@@ -1,14 +1,22 @@
-﻿using System;
+﻿using DataModels;
+using LinqToDB;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Windows.Forms;
+
 
 namespace Sistema_Reservaciones_G1.Pages
 {
     public partial class CrearHabitacion : System.Web.UI.Page
     {
+        string conn = ConfigurationManager.ConnectionStrings["MyDatabase"].ConnectionString;
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -16,12 +24,30 @@ namespace Sistema_Reservaciones_G1.Pages
 
         protected void ButtonRegresar_Click(object sender, EventArgs e)
         {
-
+            Response.Redirect("Gestionarhabitaciones.aspx");
         }
 
         protected void ButtonGuardarcrearhabitacion_Click(object sender, EventArgs e)
         {
+            if (Page.IsValid) { try
+                {
+                    int hotelId = Convert.ToInt32(droplisthoteles.SelectedValue);
+                    string numeroHabitacion = txtnumhabitacion.Text;
+                    int capacidadMaxima = Convert.ToInt32(DropDownListcapacidad.SelectedValue);
+                    string descripcion = Textdescrip.Text;
+                    using (PvProyectoFinalDB db = new PvProyectoFinalDB(new DataOptions().UseSqlServer(conn)))
+                    {
+                        db.SpConsultarHabitaciones(hotelId,numeroHabitacion,capacidadMaxima,descripcion).FirstOrDefault();
 
+                    }
+                }
+                catch { 
+                
+                Response.Redirect("Error");
+                
+                }
+           //se debe agregar un validador de pagina ?
+            }
         }
     }
 }
